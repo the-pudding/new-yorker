62de525757f6777b00589d5b597527f787a38edc
Russell Goldenberg <russellgoldenberg@gmail.com>
2021-05-06 14:49:23 -0400

    Initialize kit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "$styles/app.css";
+  import Header from "$components/Header.svelte";
+</script>
+
+<main id="content">
+  <Header />
+
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Meta from "$components/Meta.svelte";
+  import App from "$components/App.svelte";
+</script>
+
+<Meta />
+<App />
